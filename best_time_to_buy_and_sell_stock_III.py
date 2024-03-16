a3c60bf80c7db5a745770fb47f329e74e3b8959a
Kothamasu Venkata Naga Sai Guru Charan <96035230+kgurucharan2003@users.noreply.github.com>
2024-03-16 18:12:12 +0530

    best_time_to_buy_and_sell_stock_III.py

--- best_time_to_buy_and_sell_stock_III.py
+++ best_time_to_buy_and_sell_stock_III.py
@@ -5,10 +5,8 @@ class Solution:
     buy1, buy2 = float('inf'), float('inf')
     sell1, sell2 = 0, 0
     for price in prices:
-        # update first buy and sell values
         buy1 = min(buy1, price)
         sell1 = max(sell1, price - buy1)
-        # update second buy and sell values
         buy2 = min(buy2, price - sell1)
         sell2 = max(sell2, price - buy2)
     return sell2
