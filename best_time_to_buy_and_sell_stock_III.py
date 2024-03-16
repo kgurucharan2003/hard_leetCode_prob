9394dcc2d5cd6a6860f5bf9d927a853f7f34f5d0
Kothamasu Venkata Naga Sai Guru Charan <96035230+kgurucharan2003@users.noreply.github.com>
2024-03-16 18:08:24 +0530

    Update best_time_to_buy_and_sell_stock_III.py

--- best_time_to_buy_and_sell_stock_III.py
+++ best_time_to_buy_and_sell_stock_III.py
@@ -2,12 +2,8 @@ class Solution:
   def maxProfit(self, prices: List[int]) -> int:
     if not prices:
         return 0
-
-    # initialize variables for first buy, first sell, second buy, and second sell
     buy1, buy2 = float('inf'), float('inf')
     sell1, sell2 = 0, 0
-
-    # iterate over prices to update buy and sell values
     for price in prices:
         # update first buy and sell values
         buy1 = min(buy1, price)
